e58328306737fd356ec862f1e573945aee34718f
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-14 15:06:14 -0400

    Created an object and asigned it an sub-object

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat", 
+    }
+
+}
+
